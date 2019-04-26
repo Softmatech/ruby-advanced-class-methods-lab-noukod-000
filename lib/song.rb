e1a3654e32df167c969935ff6adbdc6b1d7dd5e9
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-26 17:29:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,29 @@ def create_by_name(name)
     song
 end
 
+def self.find_by_name(name)
+    @@all.find  { |song|  song.name(name)  }
+end
+
+def self.find_or_create_by_name(name)
+    found = self.find_by_name(name)
+    if !found
+      found
+    else
+      self.create_by_name(name)
+    end
+end
+
+def self.new_from_filename(filename)
+    filename = filename.split(/ - /)
+    artist_name = filename[0]
+    song_name = filename[1].sub(/.mp3/,'')
+    song = Song.new(song_name, artist_name)
+    @@all << song
+end
+
+def self.destroy_all
+    @@all.clear
+end
+
 end
